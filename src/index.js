bb9df73f1d6bd0fd6a8ef91f4da9c7d617afe0e7
Elias Rayas <eliasrayasg@gmail.com>
2021-06-04 15:27:12 -0500

    Added lazy loading, log status

--- src/index.js
+++ src/index.js
@@ -1,7 +1,45 @@
-import { registerImage } from "./lazy";
+import { registerImage, log } from "./lazy";
 // const url = "https://source.unsplash.com/random/photos/";
 const url = "https://randomuser.me/api/";
 // const url = "https://randomuser.me/api/?results=10";
+
+//get app
+const app = document.getElementById("app");
+
+const addImageButton = document.getElementById("button");
+const removeImageButton = document.getElementById("remove");
+
+//add listener to addButton
+addImageButton.addEventListener("click", async () => {
+  //crear plantilla
+  log();
+  const images = createTemplate();
+  app.append(images);
+
+  //con esta funcion solicitamos los datos a la API
+  //y creamos el dataset src para pasar al observer
+  const addUrl = async () => {
+    //conseguir la informacion
+    const imageUrl = await getData(url);
+
+    //agregar el atributo dataset.src al nodo image
+    images.firstChild.dataset.src = await imageUrl;
+  };
+
+  await addUrl();
+
+  registerImage(images);
+});
+
+//listener for removeButton
+removeImageButton.addEventListener("click", () => {
+  //Seleccionamos #app y le decimos que por cada nodo dentro de app, lo remueva
+  app.querySelectorAll("div").forEach((child) => {
+    child.remove();
+  });
+});
+
+//----functions----//
 
 //fetch data function
 async function getData(url) {
@@ -9,16 +47,12 @@ async function getData(url) {
   const response = await request.json();
   const data = await response.results;
 
-  const mappedData = data.map((item) => {
-    return item.picture.large;
-  });
-
-  //return array of strings (urls)
-  return mappedData[0];
+  //return a string
+  return data[0].picture.large;
 }
 
 //create template function
-function createTemplate(src) {
+function createTemplate() {
   //create parent div
   const div = document.createElement("div");
 
@@ -30,46 +64,11 @@ function createTemplate(src) {
 
   //style img node
   img.style.width = "320px";
-  img.style.height = "auto";
+  img.style.height = "400px";
   img.className = "mx-auto bg-gray-200";
-
-  //load source
-  //update: cambiamos la propiedad src a dataset src, para despues con lazy loading
-  //acceder al dataset y setear la propiedad src
-  img.dataset.src = src;
 
   //append node
   div.appendChild(img);
 
   return div;
 }
-
-//we create an array of images using the data and the templating function
-async function createImages() {
-  //get array of strings
-  const imagesUrl = await getData(url);
-  const renderedImage = createTemplate(imagesUrl);
-
-  return renderedImage;
-}
-
-//get app
-const app = document.getElementById("app");
-
-//create button
-const addImageButton = document.createElement("button");
-addImageButton.className =
-  "w-40 mx-auto my-8 p-2 text-center text-white bg-purple-400 hover:bg-purple-600 rounded-lg";
-addImageButton.innerText = "Add image";
-
-//add listener to button
-addImageButton.addEventListener("click", async () => {
-  //we fetch api images
-  const getImages = await createImages();
-  //we append the images to the app
-  app.append(getImages);
-  registerImage(getImages);
-});
-
-//append button, images to app
-app.append(addImageButton);
--- src/lazy.js
+++ src/lazy.js
@@ -1,9 +1,20 @@
 //intersection observer
 
+//counters for logs
+let reqImage = 0;
+let loadedImage = 0;
+
 //Esta funcion recibe una entrada en la que revisa si el elemento se encuantra dentro del viewport o no
 const isIntersecting = (entry) => {
   //retornamos falso o verdadero para aplicar en el filtro
   return entry.isIntersecting;
+};
+
+export const log = () => {
+  console.log(`
+    Requested images: ${reqImage},
+    Loaded images: ${loadedImage},
+  `);
 };
 
 //Instanciamos a un IntersectionObserver
@@ -11,6 +22,10 @@ const observer = new IntersectionObserver((entries) => {
   //Filtramos las entradas que sean visibles y por cada una
   //ejecutamos una funcion
   entries.filter(isIntersecting).forEach((entry) => {
+    loadedImage++;
+    log();
+    //aqui ejecutamos las acciones una vez que se intersecta el elemento
+
     //referimos el nodo (DIV) que esta disparando el efecto usando entry como referencia
     //entry = imagen que cargamos
     const node = entry.target;
@@ -19,9 +34,9 @@ const observer = new IntersectionObserver((entries) => {
     const image = node.firstChild;
 
     //le asignamos al atributo src el valor del dataset que creamos junto con la imagen
+    // cambiamos las propiedades
     image.src = image.dataset.src;
-    //aqui ejecutamos las acciones una vez que se intersecta el elemento
-    // console.log(entry);
+    image.style.height = "auto";
 
     //le decimos al observer que deje de observar al nodo que dispara el evento
     observer.unobserve(node);
@@ -31,5 +46,6 @@ const observer = new IntersectionObserver((entries) => {
 //registramos una imagen cuando aparezca en el viewport
 export const registerImage = (image) => {
   //suscribimos las imagenes
+  reqImage++;
   observer.observe(image);
 };
